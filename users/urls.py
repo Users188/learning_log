ae7fea24d9e1a11c232dfbe130c2968f8d1eb64d
Users188 <43400284+Users188@users.noreply.github.com>
2020-10-04 22:34:44 +0800

    fix bugs&add logout
    修复了不显示用户名，登录登出链接的bug
    增加了注销功能

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,10 @@ from django.contrib.auth.views import LoginView
 
 from . import views
 
-urlpatterns =[
-    #登录页面
-    url(r'^login/$',LoginView.as_view(template_name='users/login.html'),name='login')
+urlpatterns = [
+    # 登录页面
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # 注销
+    url(r'^logout/$', views.logout_view, name='logout'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.urls import reverse
+
 
 # Create your views here.
+
+def logout_view(request):
+    """退出登录"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))

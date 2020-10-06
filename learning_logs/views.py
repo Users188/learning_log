dfeccef43cd8035fb0b6de8987af01a94c231cdb
Users188 <43400284+Users188@users.noreply.github.com>
2020-10-06 18:14:09 +0800

    保护页面new_entry
    保存条目前调用check_topic_owner()保证加入的条目 是当前用户的条目

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -67,6 +67,7 @@ def new_entry(request, topic_id):
         if form.is_valid():
             new_entry = form.save(commit=False)
             new_entry.topic = topic
+            check_topic_owner(request,topic)
             new_entry.save()
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
     context = {'topic': topic, 'form': form}
